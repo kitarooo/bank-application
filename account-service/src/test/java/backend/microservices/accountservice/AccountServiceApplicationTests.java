84ebec9a432d5390a8df1e091798ac3e7cae81a7
azamatshauenbekov <bravlstarstop05@mail.ru>
2024-06-28 16:12:49 +0500

    sfsf

---
+++ account-service/src/test/java/backend/microservices/accountservice/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.microservices.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
